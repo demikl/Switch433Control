80b9b8a69a28c4fd3ae2feed8f56cb26d2220873
Mickaël Le Baillif <mickael.le.baillif@gmail.com>
2020-06-12 17:06:57 +0200

    use Homie-ESP as base framework for MQTT communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,223 +1,152 @@
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
+#include <Homie.h>
 #include <RCSwitch.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-#include "settings.h"
 #include "switches.h"
 
-void setup_433();
-void setup_mqtt();
-void setup_wifi();
+#define firmwareVersion "1.0.0"
 
-void led_feedback(uint16 on_msec, uint16 off_msec, uint8 nbloops);
-void mqtt_callback(char* topic, byte* payload, unsigned int length);
+#define FW_NAME "rf433_homie"
+#define FW_VERSION "1.0.0"
 
-WiFiClient wifi;
-PubSubClient mqtt(wifi);
-RCSwitch mySwitch = RCSwitch();
-long last_pub_ts = 0;
-bool need_mqtt_refresh = false;
+/* Magic sequence for Autodetectable Binary Upload */
+const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
+const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
+/* End of magic sequence for Autodetectable Binary Upload */
 
-#define NB_SWITCHES 5
-bool switches_state[NB_SWITCHES];
+#define PIN433_RCV D5
+#define PIN433_SND D2
+#define ONE_WIRE_BUS D1
+#define ONE_WIRE_PULLUP D6
 
-void setup() {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+// ADC_MODE(ADC_VCC);
 
-  // For using serial terminal on computer, for debugging
-  Serial.begin(9600);
-  
-  setup_433();
-  setup_wifi();
-  setup_mqtt();
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
 
-  for (uint8 i = 0; i < NB_SWITCHES; i++) switches_state[i] = false;
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature oneWireSensors(&oneWire, ONE_WIRE_PULLUP);
+
+const unsigned int NUMBER_OF_SWITCHES = 5;
+const int DEFAULT_SENSORS_INTERVAL = 300;
+unsigned long lastSensorsSent = 0;
+
+RCSwitch rfSwitch = RCSwitch();
+HomieNode rfSwitchesNode("switches", "Switches", "switch", true, 1, NUMBER_OF_SWITCHES);
+HomieNode temperatureNode("temperature", "Temperature", "temperature");
+HomieNode vccNode("powerSupply", "Power Supply", "generic");
+HomieNode brightnessNode("brightness", "Ambiant luminescence", "generic");
+HomieSetting<long> sensorsIntervalSetting("sensorsInterval", "Interval in seconds between each sensor update to MQTT");
+
+void loopHandler() {
+  if (millis() - lastSensorsSent >= sensorsIntervalSetting.get() * 1000UL || lastSensorsSent == 0) {
+    lastSensorsSent = millis();
+
+    // disabled because ADC is used by photoresistor
+    if (false) {
+      uint16_t voltage = ESP.getVcc();
+      Homie.getLogger() << "Voltage: " << voltage << endl;
+      vccNode.setProperty("powerSupply").send(String(voltage));
+    }
+
+    // Photoresistor
+    int adc = analogRead(A0);
+    brightnessNode.setProperty("adc_1024").send(String(adc));
+
+    Homie.getLogger() << "One-wire bus:" << endl;
+    Homie.getLogger() << "  |-- discovered devices: " << oneWireSensors.getDeviceCount() << endl;
+    Homie.getLogger() << "  |-- DS1820 devices:     " << oneWireSensors.getDS18Count() << endl;
+    float tempC = oneWireSensors.getTempCByIndex(0);
+    if (tempC != DEVICE_DISCONNECTED_C) {
+      Homie.getLogger() << "Temperature: " << tempC << " °C" << endl;
+      temperatureNode.setProperty("degrees").send(String(tempC));
+    }
+    else
+    {
+      Homie.getLogger() << "Error reading temperature" << endl;
+    }
+    // préparer le prochain relevé de température
+    oneWireSensors.requestTemperatures();
+
+  }
+
+  if (rfSwitch.available()) {
+    unsigned long code = rfSwitch.getReceivedValue();
+    rfSwitch.resetAvailable();
+
+    uint8 i;
+    for (i = 0; i < NUMBER_OF_SWITCHES; i++)
+    {
+      if (code == switches[i].on)
+      {
+        Homie.getLogger() << "Switch " << i+1 << " has been turned ON by RF remote control" << endl;
+        rfSwitchesNode.setProperty("on").setRange(i+1).send("on");
+        break;
+      } else if (code == switches[i].off) {
+        Homie.getLogger() << "Switch " << i+1 << " has been turned OFF by RF remote control" << endl;
+        rfSwitchesNode.setProperty("on").setRange(i+1).send("off");
+        break;
+      }
+    }
+    // unknown code
+    if (i == NUMBER_OF_SWITCHES) {
+      Homie.getLogger() << "Unknown RC433 code received on RF: " << code << endl;
+    }
+  }
+}
+
+bool switchHandler(const HomieRange& range, const String& value) {
+  if (!range.isRange) return false;  // if it's not a range
+  if (range.index < 1 || range.index > NUMBER_OF_SWITCHES) return false;  // if it's not a valid range
+  if (value != "on" && value != "off") return false;  // if the value is not valid
+
+  bool on = (value == "on");
+  SwitchCodes rf_codes = switches[range.index - 1];
+  rfSwitch.send(on ? rf_codes.on : rf_codes.off, 24);
+
+  rfSwitchesNode.setProperty("on").setRange(range).send(value);
+  Homie.getLogger() << "Switch " << range.index << " is " << value << endl;
+  return true;
 }
 
 void setup_433(){
-  // Transmitter is connected to NodeMCU pin D3 / GPIO#4
-  // Receiver is on D0 / GPIO#0
-  mySwitch.enableTransmit(4);
-  mySwitch.enableReceive(0);
+  rfSwitch.enableTransmit(PIN433_SND);
+  rfSwitch.enableReceive(PIN433_RCV);
   
   // config for Youthink 433 MHz power outlets
   // https://www.amazon.fr/gp/product/B0785G3HYC
-  mySwitch.setProtocol(1);
-  mySwitch.setPulseLength(149);
+  rfSwitch.setProtocol(1);
+  rfSwitch.setPulseLength(149);
   
   // Optional set number of transmission repetitions.
   // mySwitch.setRepeatTransmit(15);
 }
 
-void setup_wifi() {
-  // Wait for WiFi module to be ready
-  led_feedback(50, 50, 10);
-  
-  Serial.println();
-  Serial.print("Connexion WiFi sur SSID ");
-  Serial.println(wifi_ssid);
+void setup() {
+  Serial.begin(115200);
+  setup_433();
+  oneWireSensors.begin();
+  oneWireSensors.requestTemperatures();
 
-  WiFi.begin(wifi_ssid, wifi_password);
+  Homie_setFirmware(FW_NAME, FW_VERSION);
+  Homie.setLoopFunction(loopHandler);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    led_feedback(50, 0, 1);
-  }
-  led_feedback(10, 40, 15);
-
-  Serial.println("");
-  Serial.println("Connexion WiFi etablie ");
-  Serial.print("=> Addresse IP : ");
-  Serial.print(WiFi.localIP());
-}
-
-void setup_mqtt() {
-  mqtt.setServer(mqtt_server, 1883);
-  mqtt.setCallback(mqtt_callback);
-}
-
-void mqtt_reconnect() {
-  while (!mqtt.connected()) {
-    Serial.print("Connexion au broker MQTT...");
-    if (mqtt.connect("ESP8266Client", mqtt_user, mqtt_password)) {
-      Serial.println("OK");
-      led_feedback(20, 80, 10);
-      mqtt.subscribe("cmnd/switch1/power");
-      mqtt.subscribe("cmnd/switch2/power");
-      mqtt.subscribe("cmnd/switch3/power");
-      mqtt.subscribe("cmnd/switch4/power");
-      mqtt.subscribe("cmnd/switch5/power");
-    } else {
-      Serial.print("echec, erreur : ");
-      Serial.print(mqtt.state());
-      Serial.println("Retrying in 5sec...");
-      led_feedback(80, 20, 20);
-      delay(3000);
-    }
-  }
-}
-
-// MQTT rcv buffer for string operations
-char mqtt_buff[100];
-
-void mqtt_callback(char* topic, byte* payload, unsigned int length) {
-  Serial.println("MQTT message received from topic: " + String(topic));
-  Serial.print(" | length: " + String(length,DEC));
-
-  // prevent buffer overflow
-  if (length >= 100) length = 99;
-
-  uint i;
-  for(i = 0; i < length ; i++) {
-    mqtt_buff[i] = payload[i];
-  }
-  mqtt_buff[i] = '\0';
-  String mqtt_msg = String(mqtt_buff);
-  Serial.println("  Payload: [" + mqtt_msg + "]");
-
-  char num = topic[11];
-  uint8 switch_idx = 0;
-  switch (num)
-  {
-  case '1':
-    switch_idx = 0;
-    break;
-  case '2':
-    switch_idx = 1;
-    break;
-  case '3':
-    switch_idx = 2;
-    break;
-  case '4':
-    switch_idx = 3;
-    break;
-  case '5':
-    switch_idx = 4;
-    break;
-  
-  default:
-    return;
-  }
-
-  if (mqtt_msg == "ON") {
-    led_feedback(1, 49, 5);
-    switches_state[switch_idx] = true;
-    mySwitch.send(switches[switch_idx].on, 24);
-  } else if (mqtt_msg == "OFF") {
-    led_feedback(35, 15, 5);
-    mySwitch.send(switches[switch_idx].off, 24);
-    switches_state[switch_idx] = false;
-  }
-  need_mqtt_refresh = true;
-}
-
-void mqtt_publish_states() {
-  for (uint8 i = 0; i < NB_SWITCHES; i++)
-  {
-    String sw_name = "switch" + String(i+1, DEC);
-    Serial.println("Publishing state for " + sw_name + (switches_state[i] ? "ON" : "OFF"));
-    mqtt.publish(("stat/" + sw_name + "/POWER").c_str(), switches_state[i] ? "ON" : "OFF");
-  }
-  
-}
-
-void rc433_loop() {
-  if (mySwitch.available()) {
-    need_mqtt_refresh = true;
-    unsigned long code = mySwitch.getReceivedValue();
-    mySwitch.resetAvailable();
-
-    uint8 i;
-    for (i = 0; i < NB_SWITCHES; i++)
-    {
-      if (code == switches[i].on)
-      {
-        switches_state[i] = true;
-        Serial.print("Switch #");
-        Serial.print(i+1);
-        Serial.println(" has turned ON");
-        break;
-      } else if (code == switches[i].off) {
-        switches_state[i] = false;
-        Serial.print("Switch #");
-        Serial.print(i+1);
-        Serial.println(" has turned OFF");
-        break;
-      }
-    }
-    // unknown code
-    if (i == NB_SWITCHES) {
-      need_mqtt_refresh = false;
-      Serial.print("Unknown RC433 code: ");
-      Serial.println(code);
-    }
-  }
-}
-
-void led_feedback(uint16 on_msec, uint16 off_msec, uint8 nbloops) {
-  for (uint8 i = 0; i < nbloops; i++)
-  {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(on_msec);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(off_msec);
-  }
+  rfSwitchesNode.advertise("on").setName("On").setDatatype("boolean").settable(switchHandler);
+  temperatureNode.advertise("degrees").setName("Degrees")
+                                      .setDatatype("float")
+                                      .setUnit("ºC");
+  vccNode.advertise("vcc").setName("VCC")
+                          .setDatatype("float")
+                          .setUnit("mV");
+  brightnessNode.advertise("adc_1024").setName("ADC conversion value - 0 to 1023")
+                                      .setDatatype("integer");
+  sensorsIntervalSetting.setDefaultValue(DEFAULT_SENSORS_INTERVAL).setValidator([] (long candidate) {
+    return candidate >= 1;
+  });
+  Homie.setup();
 }
 
 void loop() {
-  if (!mqtt.connected()) {
-    mqtt_reconnect();
-  }
-  mqtt.loop();
-  rc433_loop();
-
-  long now = millis();
-  if (need_mqtt_refresh || (now - last_pub_ts >= PUBLISH_STATES_INTERVAL)) {
-    last_pub_ts = now;
-    mqtt_publish_states();
-    need_mqtt_refresh = false;
-  }
+  Homie.loop();
 }
